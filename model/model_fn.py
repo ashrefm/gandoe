41a77bbedd02f815e0f408dabb063a1ccbf2fc51
ashrefm <ashref.maiza@gmail.com>
2019-08-05 23:54:52 +0200

    adding validation min vs training max losses

--- model/model_fn.py
+++ model/model_fn.py
@@ -15,6 +15,7 @@
 """Define the model."""
 
 
+import math
 import tensorflow as tf
 from model.loss_fn import *
 from model.models import encoder, decoder, discriminator
@@ -40,6 +41,12 @@ def model_fn(features, mode, params):
 
     img_channels = 3 if params.rgb else 1
     img_size = params.img_size
+    z_size = params.z_size
+
+    # Check if image size is power of 2
+    if not math.ceil(math.log2(img_size)) == math.floor(math.log2(img_size)):
+        raise ValueError("Image size in params files should be a power of 2. Found {}".format(img_size))
+
 
     images = tf.reshape(features, [-1, img_size, img_size, img_channels])
     assert images.shape[1:] == [img_size, img_size, img_channels], "{}"\
@@ -96,7 +103,36 @@ def model_fn(features, mode, params):
     enc_loss = l2_loss(z, z_star)
 
     # generator loss
-    gen_loss = 50 * con_loss + fma_loss + adv_loss + enc_loss
+    gen_loss = adv_loss
+
+
+    if is_training:
+        min_vs_max_enc_loss = tf.reduce_max(
+            tf.reduce_mean(
+                tf.reshape(tf.square(z - z_star), [-1, z_size]),
+                axis=1)) 
+        min_vs_max_con_loss = tf.reduce_max(
+            tf.reduce_mean(
+                tf.reshape(tf.abs(images - fakes), [-1, img_size*img_size*img_channels]),
+                axis=1))
+        min_vs_max_fma_loss = tf.reduce_max(
+            tf.reduce_mean(
+                tf.reshape(tf.square(features_real - features_fake), [-1, 4*4*(img_size*2)]),
+                axis=1))
+    else:
+        min_vs_max_enc_loss = tf.reduce_min(
+            tf.reduce_mean(
+                tf.reshape(tf.square(z - z_star), [-1, z_size]),
+                axis=1))
+        min_vs_max_con_loss = tf.reduce_min(
+            tf.reduce_mean(
+                tf.reshape(tf.abs(images - fakes), [-1, img_size*img_size*img_channels]),
+                axis=1))
+        min_vs_max_fma_loss = tf.reduce_min(
+            tf.reduce_mean(
+                tf.reshape(tf.square(features_real - features_fake), [-1, 4*4*(img_size*2)]),
+                axis=1))
+
 
     # discriminator loss
     real_loss = bce_loss(logits_real, tf.ones_like(logits_real))
@@ -137,9 +173,12 @@ def model_fn(features, mode, params):
             'z_mean_norm': tf.metrics.mean(z_mean_norm),
             'z_star_mean_norm': tf.metrics.mean(z_star_mean_norm),
             'context_loss': tf.metrics.mean(con_loss),
+            'min_vs_max_context_loss': tf.metrics.mean(min_vs_max_con_loss),
             'feature_matching_loss': tf.metrics.mean(fma_loss),
+            'min_vs_max_fma_loss': tf.metrics.mean(min_vs_max_fma_loss),
             'adversarial_loss': tf.metrics.mean(adv_loss),
             'encoder_loss': tf.metrics.mean(enc_loss),
+            'min_vs_max_encoder_loss': tf.metrics.mean(min_vs_max_enc_loss),
             'generator_loss': tf.metrics.mean(gen_loss),
             'real_loss': tf.metrics.mean(real_loss),
             'fake_loss': tf.metrics.mean(fake_loss),
@@ -156,9 +195,12 @@ def model_fn(features, mode, params):
     tf.summary.scalar("z_mean_norm", z_mean_norm)
     tf.summary.scalar("z_star_mean_norm", z_star_mean_norm)
     tf.summary.scalar("context_loss", con_loss)
+    tf.summary.scalar("min_vs_max_context_loss", min_vs_max_con_loss)
     tf.summary.scalar("feature_matching_loss", fma_loss)
+    tf.summary.scalar("min_vs_max_fma_loss", min_vs_max_fma_loss)
     tf.summary.scalar("adversarial_loss", adv_loss)
     tf.summary.scalar("encoder_loss", enc_loss)
+    tf.summary.scalar("min_vs_max_encoder_loss", min_vs_max_enc_loss)
     tf.summary.scalar("generator_loss", gen_loss)
     tf.summary.scalar("real_loss", real_loss)
     tf.summary.scalar("fake_loss", fake_loss)
@@ -181,6 +223,7 @@ def model_fn(features, mode, params):
 
     if params.optimizer in list(optimizers.keys()):
         gen_optimizer = optimizers[params.optimizer](params.learning_rate)
+        enc_optimizer = optimizers[params.optimizer](params.learning_rate)
         dis_optimizer = optimizers[params.optimizer](params.learning_rate)
     else:
         raise ValueError(
@@ -195,11 +238,10 @@ def model_fn(features, mode, params):
     # Connect the different optimizers
     train_op = tf.group(
         gen_optimizer.minimize(gen_loss, global_step=global_step),
+        enc_optimizer.minimize(enc_loss, global_step=global_step),
         dis_optimizer.minimize(dis_loss, global_step=global_step))
-
-    #train_op = gen_optimizer.minimize(gen_loss+, global_step=global_step)
     
     return tf.estimator.EstimatorSpec(
         mode,
-        loss=gen_loss+dis_loss,
+        loss=gen_loss+enc_loss+dis_loss,
         train_op=train_op)
